e5e576263e8dbe10d958fa4bf9ca1d56589424ce
GlassCol <coleglass18@outlook.com>
2022-09-14 15:53:59 -0500

    Create login and Create Account pages/Completed Styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import Home from './Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Login from './Login';
+import CreateAccount from './CreateAccount';
 
 export default function App() {
   return (
-
     <Router>
-    <div className='container'>
       <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/CreateAnAccount' element={<CreateAccount />} />
       </Routes>
-    </div>
     </Router>
 
   )
