1abcf0ae44d6d0e426827a174ac6b9910e63d19f
GlassCol <coleglass18@outlook.com>
2022-12-05 15:48:23 -0600

    End of day still in progress

--- genericAPI/genericAPI/src/main/java/com/bankapp/genericAPI/entity/AccountEntity.java
+++ genericAPI/genericAPI/src/main/java/com/bankapp/genericAPI/entity/AccountEntity.java
@@ -3,13 +3,17 @@ package com.bankapp.genericAPI.entity;
 
 
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
 @Table(name="accounts")
-public class AccountEntity {
+public class AccountEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -19,4 +23,35 @@ public class AccountEntity {
     private String lastName;
     private Double balance = 0.0;
     private String password;
+    private boolean accountNonLocked = true;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> "read");
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
